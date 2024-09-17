7b9a0c2d43c84aa12fb4d00992b6db04ddbea00a
Luciano <lu.camano@duocuc.cl>
2024-09-17 19:54:57 -0300

    Componente encabezado y home

---
+++ src/app/components/encabezado/encabezado.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-encabezado',
+  templateUrl: './encabezado.component.html',
+  styleUrls: ['./encabezado.component.scss'],
+})
+export class EncabezadoComponent  implements OnInit {
+
+  @Input()
+  titulo: string = "";
+
+  constructor() { 
+    this.titulo = this.titulo.slice(0, 1).toUpperCase() + this.titulo.slice(1);
+  }
+
+  ngOnInit() {}
+
+}
