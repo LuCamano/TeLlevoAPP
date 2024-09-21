b326117647c89030f121a88134c3134bbfca44ea
paul <paul.salinasc@duocuc.cl>
2024-09-21 17:44:37 -0300

    mejoras visuales y cambio a notificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  isLoginPage = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      // Verificar si la ruta actual es la página de login
+      this.isLoginPage = this.router.url === '/login' || this.router.url === '/login/cambiar-contra';
+      
+    });
+  }
+  
+  logout() {
+    this.router.navigate(['/login']);
+  }
 }
