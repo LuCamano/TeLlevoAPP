8259c3ded8ab88c5ce7beffc2b32714ecf9488a3
Luciano <lu.camano@duocuc.cl>
2024-09-24 05:47:05 -0300

    Mejoras y ajustes al login

--- src/app/pages/login/login-routing.module.ts
+++ src/app/pages/login/login-routing.module.ts
@@ -7,7 +7,8 @@ const routes: Routes = [
   {
     path: '',
     component: LoginPage
-  },  {
+  },
+  {
     path: 'cambiar-contra',
     loadChildren: () => import('./cambiar-contra/cambiar-contra.module').then( m => m.CambiarContraPageModule)
   },
