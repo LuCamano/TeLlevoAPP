a100d34e96f76d01486021a2fe5d0ca9a004f423
Luciano <lu.camano@duocuc.cl>
2024-10-16 00:18:36 -0300

    Firebase configurado

---
+++ src/environments/firebaseconfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBymZAYgadUbuJcHSN72RvPJ1dWx7Qk_hU",
+    authDomain: "tellevoapp-35977.firebaseapp.com",
+    projectId: "tellevoapp-35977",
+    storageBucket: "tellevoapp-35977.appspot.com",
+    messagingSenderId: "1002431398345",
+    appId: "1:1002431398345:web:00cd439ca86cd2769b6818"
+};
