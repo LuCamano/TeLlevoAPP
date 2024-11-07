0a6a825cb85f6cc0a741c60043eaf2cf5b2eae0b
Luciano <lu.camano@duocuc.cl>
2024-11-07 02:57:30 -0300

    Agregar mapa interactivo en la página de administración de viajes y ajustar estilos

--- src/app/pages/tabs/conducir/adm-viajes/adm-viajes.page.ts
+++ src/app/pages/tabs/conducir/adm-viajes/adm-viajes.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { UtilsService } from 'src/app/services/utils.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-adm-viajes',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adm-viajes.page.scss'],
 })
 export class AdmViajesPage implements OnInit {
+  // Inyectar servicios
+  private utils = inject(UtilsService);
 
-  constructor() { }
+  map!: mapboxgl.Map;
+
+  constructor() {
+    (mapboxgl as any).accessToken = environment.MAPBOX_TOKEN;
+  }
+
+  ionViewWillEnter() {
+    if (!this.map) this.buildMap();
+  }
 
   ngOnInit() {
   }
 
+  async buildMap() {
+    const coords = (await this.utils.getCurrentPosition()).coords;
+    this.map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [coords.longitude, coords.latitude],
+      zoom: 12
+    });
+    this.map.resize();
+  }
 }
