f5d34848bd8a63adedd771cb83d6c0e54ff7bd5a
Luciano <lu.camano@duocuc.cl>
2024-11-09 01:02:11 -0300

    Agregada función para mostrar la ruta en el mapa

--- src/app/pages/tabs/conducir/adm-viajes/adm-viajes.page.ts
+++ src/app/pages/tabs/conducir/adm-viajes/adm-viajes.page.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 import { UtilsService } from 'src/app/services/utils.service';
 import { environment } from 'src/environments/environment.prod';
 
@@ -15,7 +15,7 @@ export class AdmViajesPage implements OnInit {
   map!: mapboxgl.Map;
 
   constructor() {
-    (mapboxgl as any).accessToken = environment.MAPBOX_TOKEN;
+    mapboxgl.accessToken = environment.MAPBOX_TOKEN;
   }
 
   ionViewWillEnter() {
@@ -25,6 +25,38 @@ export class AdmViajesPage implements OnInit {
   ngOnInit() {
   }
 
+  obtenerRuta(start: [number, number], end: [number, number]) {
+    const url = `https://api.mapbox.com/directions/v5/mapbox/driving/${start[0]},${start[1]};${end[0]},${end[1]}?geometries=geojson&access_token=${mapboxgl.accessToken}`;
+    fetch(url)
+    .then( response => response.json())
+    .then( data => {
+      console.log(data);
+      this.map.addLayer({
+        id: 'route',
+        type: 'line',
+        source: {
+          type: 'geojson',
+          data: {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: data.routes[0].geometry.coordinates
+            }
+          }
+        },
+        layout: {
+          'line-join': 'round',
+          'line-cap': 'round'
+        },
+        paint: {
+          'line-color': '#3887be',
+          'line-width': 8
+        }
+      });
+    });
+  }
+
   async buildMap() {
     try {
       try {
@@ -32,17 +64,25 @@ export class AdmViajesPage implements OnInit {
       } catch (error) {
         await this.utils.requestPermissions();
       }
-
+      //Obtener las coordenadas actuales
       const coords = (await this.utils.getCurrentPosition()).coords;
+      //Crear el mapa
       this.map = new mapboxgl.Map({
         container: 'mapa',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [coords.longitude, coords.latitude],
-        zoom: 12
+        zoom: 17,
+        pitch: 40,
+        bearing: -17
       });
+      //Agregar el marcador para la ubicación actual
+      new mapboxgl.Marker().setLngLat([coords.longitude, coords.latitude]).addTo(this.map);
+      
       this.map.resize();
+
+      return this.obtenerRuta([coords.longitude, coords.latitude], [-73.10885995425409, -36.73195425126108]);
     } catch (error) {
-      this.utils.navigateBack();
+      return this.utils.navigateBack();
     }
   }
 }
