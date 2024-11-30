7c23cdd3e7f77dee981e026a718e71bf1ca837e1
Luciano <lu.camano@duocuc.cl>
2024-11-30 10:18:29 -0300

    simplificar la asignación de variables en el método registrar y eliminar la función de validación de contraseñas iguales

--- src/app/pages/login/register/register.page.ts
+++ src/app/pages/login/register/register.page.ts
@@ -31,24 +31,11 @@ export class RegisterPage implements OnInit {
     ]);
   }
 
-
-  validarContraseniasIguales() {
-    const pass1 = this.registroForm.value.password;
-    const pass2 = this.registroForm.value.password2;
-
-    if (pass1 !== pass2) {
-      this.registroForm.controls.password2.setErrors({ noIguales: true});
-    }
-  }
-
   async registrar() {
     if (this.registroForm.valid) {
       const loading = await this.utils.presentLoading();
       loading.present();
-      const email = this.registroForm.value.email;
-      const password = this.registroForm.value.password;
-      const nombre = this.registroForm.value.nombre;
-      const apellidos = this.registroForm.value.apellidos;
+      const { email, password, nombre, apellidos } = this.registroForm.value;
       try {
         const user = await this.authSvc.signUp(email!, password!, nombre!, apellidos!);
         if (user) {
